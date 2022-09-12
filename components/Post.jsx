b57e34aef409e8a2e19c6242963202378a674cb7
mohamed12370 <mohamedmostafa12370@gmail.com>
2022-09-12 17:40:13 +0200

    add like functionflty to the post section

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,3 +1,4 @@
+import { async } from '@firebase/util';
 import {
   ChartBarIcon,
   ChatIcon,
@@ -6,10 +7,51 @@ import {
   ShareIcon,
   TrashIcon,
 } from '@heroicons/react/outline';
-import React from 'react';
+import { HeartIcon as HeartIconFiled } from '@heroicons/react/solid';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from 'firebase/firestore';
+import { signIn, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import Moment from 'react-moment';
+import { db } from '../firebase';
 
 export default function Post({ post }) {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    const unsubscripe = onSnapshot(
+      collection(db, 'posts', post.id, 'likes'),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
+  }, [likes]);
+
+  async function likePost() {
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(doc(db, 'posts', post.id, 'likes', session?.user.uid));
+      } else {
+        setDoc(doc(db, 'posts', post.id, 'likes', session.user.uid), {
+          username: session.user.username,
+        });
+      }
+    } else {
+      signIn();
+    }
+  }
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200 overflow-hidden">
       {/* image */}
@@ -31,7 +73,7 @@ export default function Post({ post }) {
               @{post.data().username} -{' '}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline">
-              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+              <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
             </span>
           </div>
           {/* dot icon */}
@@ -47,7 +89,27 @@ export default function Post({ post }) {
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFiled
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`${hasLiked && 'text-red-600'} text-sm select-none`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
