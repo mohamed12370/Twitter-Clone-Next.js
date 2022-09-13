4da51f71442e9d324e81582310efa967f954a6d7
mohamed12370 <mohamedmostafa12370@gmail.com>
2022-09-13 16:23:11 +0200

    install and impelement react modal

--- components/Post.jsx
+++ components/Post.jsx
@@ -20,7 +20,7 @@ import { signIn, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import Moment from 'react-moment';
 import { useRecoilState } from 'recoil';
-import { modalState } from '../atom/modalAtom';
+import { modalState, postIdState } from '../atom/modalAtom';
 import { db, storage } from '../firebase';
 
 export default function Post({ post }) {
@@ -28,6 +28,7 @@ export default function Post({ post }) {
   const [likes, setLikes] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
 
   useEffect(() => {
     const unsubscripe = onSnapshot(
@@ -101,7 +102,14 @@ export default function Post({ post }) {
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon
-            onClick={() => setOpen(!open)}
+            onClick={() => {
+              if (!session) {
+                signIn();
+              } else {
+                setPostId(post.id);
+                setOpen(!open);
+              }
+            }}
             className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
           />
           {session?.user.uid === post?.data().id && (
