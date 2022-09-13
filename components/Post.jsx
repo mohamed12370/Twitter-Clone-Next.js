22025bf53166f9e6f720cc9f3c87419628eef204
mohamed12370 <mohamedmostafa12370@gmail.com>
2022-09-13 13:07:52 +0200

    add delete method to the post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -15,10 +15,11 @@ import {
   onSnapshot,
   setDoc,
 } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
 import { signIn, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import Moment from 'react-moment';
-import { db } from '../firebase';
+import { db, storage } from '../firebase';
 
 export default function Post({ post }) {
   const { data: session } = useSession();
@@ -49,6 +50,13 @@ export default function Post({ post }) {
       }
     } else {
       signIn();
+    }
+  }
+
+  async function deletePost() {
+    if (window.confirm('Are you Sure you want to delete this post ?')) {
+      deleteDoc(doc(db, 'posts', post.id));
+      deleteObject(ref(storage, `posts/${post.id}/image`));
     }
   }
 
@@ -88,7 +96,12 @@ export default function Post({ post }) {
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-          <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          {session?.user.uid === post?.data().id && (
+            <TrashIcon
+              onClick={deletePost}
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            />
+          )}
           <div className="flex items-center">
             {hasLiked ? (
               <HeartIconFiled
